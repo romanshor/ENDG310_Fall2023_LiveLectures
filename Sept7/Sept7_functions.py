5e9c99793ea39953555e2dac30d33bb3e0177833
Roman Shor <roman.shor@ucalgary.ca>
2023-09-07 11:52:38 -0600

    Updated Sept 7 notes with ballistic trajectory

--- Sept7/Sept7_functions.py
+++ Sept7/Sept7_functions.py
@@ -13,3 +13,24 @@ def fib(n):
         a, b = b, a+b
     return a    # return the last number in the sequence
 
+
+
+def plot_ballistic_trajectory(v_0):
+    """
+    A function that plots the ballistic trajectory of a projectile
+
+    Inputs:
+        v_0: initial velocity of the projectile
+
+    """
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    g = 9.81
+    t = np.linspace(0, 2*v_0/g, 100)
+    y = v_0*t - 0.5*g*t**2
+
+    plt.plot(t, y)
+    plt.xlabel('time (s)')
+    plt.ylabel('height (m)')
+    plt.show()

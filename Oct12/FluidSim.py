9dd171c8ca4a3238c4d30e1a0a3d0c858d9db119
Roman Shor <roman.shor@ucalgary.ca>
2023-10-12 12:00:35 -0600

    Added fluid simulator to Oct 12th lecture

---
+++ Oct12/FluidSim.py
@@ -0,0 +1,158 @@
+"""
+A module for simulating fluid flow between tanks using pipes.
+
+"""
+
+import numpy as np
+
+class Fluid:
+    """
+    A class describing a typical fluid
+    """
+
+    def __init__(self, density, viscosity):
+        """
+        Initialize the fluid with density and viscosity
+
+        Parameters
+        ----------
+        density : float
+            The density of the fluid [kg/m^3]
+        viscosity : float
+            The viscosity of the fluid [kg/m/s]
+        """
+        self.density = density
+        self.viscosity = viscosity
+
+
+
+class Pipe:
+    """
+    A class describing a pipe with dimensions
+    """
+
+    def __init__(self, length, diameter, fluid, friction_factor=0.0119):
+        """
+        Intiialize a pipe of a certain length and diameter
+
+        Parameters
+        ----------
+        length : float
+            The length of the pipe [m] 
+        diameter : float
+            The diameter of the pipe [m]
+        fluid : Fluid
+            The fluid flowing through the pipe
+        friction_factor : float
+            The friction factor of the pipe
+            The default value is 0.0119 for a smooth pipe
+        """
+
+        self.length = length
+        self.diameter = diameter
+        self.fluid = fluid
+        self.friction_factor = friction_factor
+        self.cross_sectional_area = np.pi * (diameter / 2)**2
+
+    def pressure_drop(self, Q):
+        """
+        Calculate the pressure drop across the pipe
+        
+        Parameters
+        ----------
+        Q : float
+            The volumetric flow rate through the pipe [m^3/s]
+        """
+        return 0
+    
+    def flow_rate(self, deltaP):
+        """
+        Calculate the flow rate through the pipe
+        
+        Parameters
+        ----------
+        deltaP : float
+            The pressure drop across the pipe [Pa]
+        """
+        return 0
+    
+    def isTurbulent(self, Q):
+        """
+        Determine if the flow through the pipe is turbulent
+
+        Parameters
+        ----------
+        Q : float
+            The volumetric flow rate through the pipe [m^3/s]
+        """
+
+        return False
+    
+
+class Tank:
+    """
+    A class describing a tank with dimensions
+    """
+
+    def __init__(self, height, diameter, fluid, fluid_height, inlet_height, outlet_height):
+        """
+        Intiialize a tank of a certain height and diameter
+
+        Parameters
+        ----------
+        height : float
+            The height of the tank [m] 
+        diameter : float
+            The diameter of the tank [m]
+        fluid : Fluid
+            The fluid in the tank
+        fluid_height : float
+            The height of the fluid in the tank [m]
+        inlet_height : float
+            The height of the inlet pipe [m]
+        outlet_height : float
+            The height of the outlet pipe [m]
+        """
+
+        self.height = height
+        self.diameter = diameter
+        self.fluid = fluid
+        self.fluid_height = fluid_height
+        self.inlet_height = inlet_height
+        self.outlet_height = outlet_height
+        self.tank_area = np.pi * (diameter / 2)**2
+
+
+    def outlet_pressure(self):
+        """
+        Calculate the pressure at the outlet of the tank
+        """
+        return 0
+    
+    def inlet_pressure(self):
+        """
+        Calculate the pressure at the inlet of the tank
+        """
+        return 0
+    
+    def drain_tank(self, V):
+        """
+        Drain the tank of a certain volume
+
+        Parameters
+        ----------
+        V : float
+            The volume to drain from the tank [m^3]
+        """
+        return 0
+    
+    def fill_tank(self, V):
+        """
+        Fill the tank with a certain volume
+
+        Parameters
+        ----------
+        V : float
+            The volume to fill the tank with [m^3]
+        """
+        return 0
